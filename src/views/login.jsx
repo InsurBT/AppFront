568e0286439d40bd127960f8a548eba9a1b27c36
TarekRahal <tarekrahale@gmail.com>
2020-06-26 09:53:57 +0200

    modificatin dans les vues

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -5,22 +5,30 @@ import FormContainer from '../components/form-container';
 import utilisateurService from '../service/utilisateur-service';
 
 import ConnectedUserContext from '../context/connected-user.context';
-import { TextField } from '@material-ui/core';
+import { TextField,CircularProgress } from '@material-ui/core';
 
 export default function Login(props) {
-
+    // etat des donnees entree dans les champs du formulaire
     const [credentials, setCredentials] = useState({username: '', password: ''});
+
+    // etat du message d'erreur
     const [invalidMessage, setInvalidMessage] = useState("");
+
+    // etat du chargement de la reponse a partir du serveur
+    const [loading, setLoading] = useState(false);
 
     const {setConnectedUser} = useContext(ConnectedUserContext);
 
     function submit(e) {
         e.preventDefault();
+
+        setLoading(true);
+
         if (valid()) {
             utilisateurService.connect(credentials).then(res => {
                 console.log(res);
                 if (typeof res === "string") {
-                    setInvalidMessage(res);
+                    setInvalidMessage("login ou mot de passe invalide");
                 } else {
                     setConnectedUser(res);
                     props.history.push("/home");
@@ -28,9 +36,10 @@ export default function Login(props) {
             }).catch(err => {
                 setInvalidMessage("impossible de se connecter au serveur");
                 console.log(err);
-            });
+            }).finally(() => {setLoading(false)});
         } else
             setInvalidMessage("Vous devez remplire des 2 champs pour vous connecter");
+
     }
 
     function valid() {
@@ -47,6 +56,7 @@ export default function Login(props) {
                 value={credentials.password}
                 onChange={(e) => {setCredentials({...credentials, password: e.target.value})}}
             />
+            {loading? <CircularProgress style={{margin: "auto"}} /> : ""}
             <span style={{color: 'red'}}>{invalidMessage}</span>
         </FormContainer>
     </div>);
