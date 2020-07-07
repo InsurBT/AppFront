dc1a53c3ca250843c3a6ea3ebc15531ed406461f
TarekRahal <tarekrahale@gmail.com>
2020-07-07 15:57:30 +0200

    logut

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -78,59 +78,70 @@ const styles = theme => ({
         <a.div style={animation}>
     {loading ? <div> chargement...... </div> : 
         <div >
-           <div class="cardgeneral">
-           <div class="card profile-sidebar">
-            <img src="/CNSS.png" alt="" class="profile-image"/>
-            </div>
-            <div class="card2" >
-            <Timeline align="alternate">
-            <TimelineItem>
-                <TimelineSeparator>
-                <TimelineDot color="primary">
-                    <AccountCircleRoundedIcon />
-                </TimelineDot>
-                <TimelineConnector className={classes.secondaryTail}/>
-                </TimelineSeparator>
-                <TimelineContent>
-                <Paper elevation={3} className={classes.paper}>
-                    <Typography className={classes.Typography1} variant="h6" component="h1">
-                    Nom et Prenom
-                    </Typography>
-                    <Typography className={classes.Typography2}>{connectedUser.nomComplet}</Typography>
-                </Paper>
-                </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineSeparator>
-                <TimelineDot color="primary">
-                    <LocationOnRoundedIcon />
-                </TimelineDot>
-                <TimelineConnector className={classes.secondaryTail}/>
-                </TimelineSeparator>
-                <TimelineContent>
-                <Paper elevation={3} className={classes.paper}>
-                    <Typography className={classes.Typography1} variant="h6" component="h1">
-                    Agence
-                    </Typography>
-                    <Typography className={classes.Typography2}> {connectedUser.agence} </Typography>
-                </Paper>
-                </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineSeparator>
-                <TimelineDot color="primary" variant="outlined">
-                    <AccessibilityNewRoundedIcon />
-                </TimelineDot>
-                <TimelineConnector className={classes.secondaryTail} />
-                </TimelineSeparator>
-                <TimelineContent>
-                <Paper elevation={3} className={classes.paper}>
-                    <Typography className={classes.Typography1} variant="h6" component="h1">{connectedUser.role} </Typography>
-                </Paper>
-                </TimelineContent>
-            </TimelineItem>
-            </Timeline>
-        </div>
+            <div class="cardgeneral">
+                <div class="card profile-sidebar">
+                    <img src="/CNSS.png" alt="" class="profile-image"/>
+                </div>
+                <div class="card2" >
+                    {
+                        connectedUser ?
+                        <Timeline align="alternate">
+                        <TimelineItem>
+                            <TimelineSeparator>
+                                <TimelineDot color="primary">
+                                    <AccountCircleRoundedIcon />
+                                </TimelineDot>
+
+                                <TimelineConnector className={classes.secondaryTail}/>
+                            </TimelineSeparator>
+
+                            <TimelineContent>
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.Typography1} variant="h6" component="h1">
+                                    Nom et Prenom
+                                    </Typography>
+                                    <Typography className={classes.Typography2}>{connectedUser.nomComplet}</Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+
+                        <TimelineItem>
+                            <TimelineSeparator>
+                                <TimelineDot color="primary">
+                                    <LocationOnRoundedIcon />
+                                </TimelineDot>
+
+                                <TimelineConnector className={classes.secondaryTail}/>
+                            </TimelineSeparator>
+
+                            <TimelineContent>
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.Typography1} variant="h6" component="h1">
+                                    Agence
+                                    </Typography>
+                                    <Typography className={classes.Typography2}> {connectedUser.agence} </Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+
+                        <TimelineItem>
+                            <TimelineSeparator>
+                            <TimelineDot color="primary" variant="outlined">
+                                <AccessibilityNewRoundedIcon />
+                            </TimelineDot>
+                                <TimelineConnector className={classes.secondaryTail} />
+                            </TimelineSeparator>
+
+                            <TimelineContent>
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.Typography1} variant="h6" component="h1">{connectedUser.role} </Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+                    </Timeline> :
+                    null
+                    }
+                </div>
         </div>
     </div>
     }
